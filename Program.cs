9e10780887cbcbc0e0f816a1f6614d531c093a15
Chris Can't <christiangcanturias@gmail.com>
2024-12-14 18:16:44 +0800

    Minecraft: Terminal Edition — v0.2.0

--- Program.cs
+++ Program.cs
@@ -1,35 +1,112 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class Hero {
-    public string Name;
-    public int Health;
-    public int Damage;
+class Mob {
+    public string Name { get; private set; }
+    public string Type { get; private set; }
+    public int HealthPoints { get; private set; }
+
+    public Mob(string name, string type, int healthPoints) {
+        Name = name;
+        Type = type;
+        HealthPoints = healthPoints;
+    }
+    
+    public virtual void DisplayInfo() {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Type: " + Type);
+        Console.WriteLine("Health Points: " + HealthPoints + " HP");
+    }
 }
 
-class Marksman : Hero {
-    public int Range;
-    public double CriticalChance;
+class Hostile : Mob {
+    public int Damage { get; private set; }
+    
+    public Hostile(string name, string type, int healthPoints, int damage)
+    :base(name, type, healthPoints) {
+        Damage = damage;    
+    }
+    
+    public override void DisplayInfo() {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("(New Hostile)");
+        Console.ResetColor();
+        base.DisplayInfo();
+        Console.WriteLine("Damage: " + Damage  + " ATK");
+        Console.WriteLine();
+    }
+
 }
 
-class Tank : Hero {
-    public int Armour;
-    public int Stun;
+class Passive : Mob {
+    public int HealthDrops { get; private set; }
+
+    public Passive(string name, string type, int healthPoints, int healthDrops)
+    :base(name, type, healthPoints) {
+        HealthDrops = healthDrops;    
+    }
+    
+    public override void DisplayInfo() {
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("(New Passive)");
+        Console.ResetColor();
+        base.DisplayInfo();
+        Console.WriteLine("Health Drops: " + HealthDrops + " HP");
+        Console.WriteLine();
+    }
+}
+
+class Neutral : Mob {
+    public int Damage { get; private set; }
+    public bool IsAggressive { get; private set;}
+
+    public Neutral(string name, string type, int healthPoints, int damage, bool isAggressive)
+    :base(name, type, healthPoints) {
+        Damage = damage;
+        IsAggressive = isAggressive;
+    }
+    
+    public override void DisplayInfo() {
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine("(New Neutral)");
+        Console.ResetColor();
+        base.DisplayInfo();
+        Console.WriteLine("Damage: " + Damage + " ATK");
+        Console.WriteLine("Aggressive: " + IsAggressive);
+        Console.WriteLine();
+    }
 }
 
 class Program {
     static void Main(string[] args) {
-        Marksman hero001 = new Marksman();
-        hero001.Name = "Layla";
-        hero001.Health = 2000;
-        hero001.Damage = 100;
-        hero001.Range = 250;
-        hero001.CriticalChance = 0.2;
+        Console.Title = "Minecraft: Terminal Edition (Beta)";
+
+        Mob test001 = new Hostile(
+            "Chris",
+            "Creeper",
+            20,
+            100
+        );
+
+        Mob test002 = new Passive(
+            "Jerome",
+            "Chicken",
+            4,
+            2
+        );
+
+        Mob test003 = new Neutral(
+            "Darryl",
+            "Enderman",
+            40,
+            4,
+            false
+        );
         
-        Console.WriteLine(hero001.Name);
-        Console.WriteLine(hero001.Health);
-        Console.WriteLine(hero001.Damage);
-        Console.WriteLine(hero001.Range);
-        Console.WriteLine(hero001.CriticalChance);
+        test001.DisplayInfo();
+        test002.DisplayInfo();
+        test003.DisplayInfo();
+
+        Console.ReadKey();
     }
 }
